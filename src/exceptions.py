8a8e59f518eb01df08e1c2e7e51a38cd63186bdf
LukeMurphey <Luke@LukeMurphey.net>
2017-11-10 17:51:41 -0600

    Adding some comments

--- src/exceptions.py
+++ src/exceptions.py
@@ -2,4 +2,7 @@
 This module declares all of the custom exceptions used by the modular input.
 """
 class FieldValidationException(Exception):
+    """
+    Represents the case where a field failed input validation.
+    """
     pass
