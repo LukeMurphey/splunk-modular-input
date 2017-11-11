1f991d84d1387deee6369287d0f96cd9fc5fbf09
LukeMurphey <Luke@LukeMurphey.net>
2017-11-11 01:06:21 -0600

    Adding ability for tests to run themselves only against the system or Splunk python as appropriate

--- test/unit.py
+++ test/unit.py
@@ -9,25 +9,60 @@ sys.path.append(os.path.join("..", "src"))
 
 from universal_forwarder_compatiblity import UF_MODE, make_splunkhome_path
 
+def runOnlyIfSplunkPython(func):
+    def _decorator(self, *args, **kwargs):
+        try:
+            import splunk
+            return func(self, *args, **kwargs)
+        except ImportError:
+            self.skipTest('Skipping test since this is on system Python')
+            return
+
+    return _decorator
+
+def runOnlyIfSystemPython(func):
+    def _decorator(self, *args, **kwargs):
+        try:
+            import splunk
+            self.skipTest('Skipping test since this is on Splunk Python')
+            return
+        except ImportError:
+            return func(self, *args, **kwargs)
+
+    return _decorator
+
 class TestShortcuts(unittest.TestCase):
     """
     Test the shortcuts module that provides some generic helpers.
     """
 
+    @runOnlyIfSplunkPython
     def test_is_uf_mode(self):
         """
         Make sure the UF_MODE variable can be imported.
         """
-
+        print "UF_MODE:%r" % UF_MODE
         self.assertEquals(UF_MODE, False)
+
+    @runOnlyIfSystemPython
+    def test_is_uf_mode_system(self):
+        """
+        Make sure the UF_MODE variable can be imported.
+        """
+        print "UF_MODE:%r" % UF_MODE
+        self.assertEquals(UF_MODE, True)
 
     def test_make_splunkhome_path_builtin(self):
         """
         Ensure that make_splunkhome_path works using the built-in function.
         """
 
+        if 'SPLUNK_HOME' not in os.environ:
+            os.environ['SPLUNK_HOME'] = '/opt/splunk'
+
         self.assertTrue(make_splunkhome_path(['var', 'log', 'splunk', 'test.log'], True).endswith('/var/log/splunk/test.log'))
 
+    @runOnlyIfSplunkPython
     def test_make_splunkhome_path_default(self):
         """
         Ensure that make_splunkhome_path works using core Splunk's function.
