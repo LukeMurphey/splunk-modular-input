a74581adb0363bc4a2e25edce544da2a119da083
LukeMurphey <Luke@LukeMurphey.net>
2017-11-11 01:18:20 -0600

    Moving the info module into the main module __init__

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,9 @@
 # compatibility with older modulat inputs that used the single file modular input
 from modular_input_base_class import *
 from fields import *
+
+"""
+Below is the version of this library. This copy was built on '${build.date}'. The identifier is
+'${build.number}'
+"""
+__version__ = '${version.number}'
--- src/info.py
+++
@@ -1,3 +0,0 @@
-BUILD_NUMBER = '${build.number}'
-BUILD_DATE = '${build.date}'
-VERSION = '${version.number}'
